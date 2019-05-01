779143081b14ba86de5f1f400cc23270d8f3ba61
Popka <foxxoma@gmail.com>
2019-05-01 20:50:13 +0300

    Canvas
    compass design

---
+++ script.js
@@ -0,0 +1,38 @@
+const canv = document.getElementById('canvas'),
+ctxN = canv.getContext('2d'),
+ctxS = canv.getContext('2d'),
+arc = canv.getContext('2d');
+
+canv.width = window.innerWidth;
+canv.height = window.innerHeight;
+
+
+const x = canv.width / 2, 
+y = canv.height * (85 / 100);
+
+// circle
+
+arc.fillStyle = '#fff';
+arc.arc(x, y, 70, 0, Math.PI * 2);
+arc.fill();
+arc.lineWidth = 5;
+arc.strokeStyle = 'green';
+arc.stroke();
+
+// arrowN
+
+ctxN.beginPath();
+ctxN.strokeStyle = '#0000ff';
+ctxN.moveTo(x, y);
+ctxN.lineTo(x, y - 60);
+ctxN.stroke();
+
+
+// arrowS
+
+ctxN.beginPath();
+ctxS.strokeStyle = '#ff0000';
+ctxS.moveTo(x, y);
+ctxS.lineTo(x, y + 60);
+ctxS.stroke();
+
