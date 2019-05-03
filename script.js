f26b4f5eaa37f250a581615342bef32c81ef1805
Popka <foxxoma@gmail.com>
2019-05-03 14:04:06 +0300

    3

--- script.js
+++ script.js
@@ -20,31 +20,28 @@ north.lineTo(x, y - 60);
 north.stroke();
 
 
-let rotateDegrees = 60;
 
-rotateArrow();
+
 
 
 const P = document.getElementById('p');
+
 if (window.DeviceOrientationEvent) {
     window.addEventListener("deviceorientation", function(event) { 
     	
         rotateDegrees = event.alpha; 
        	P.textContent = rotateDegrees;
-  		rotateArrow(rotateDegrees);
+       	
+       	let a = (-1 *  event.alpha) * (Math.PI/180);
+       	north.beginPath();
+		north.strokeStyle = '#0000ff';
+		north.moveTo(x, y);
+		north.lineTo(x - Math.sin(a)*60, y - Math.cos(a)*60);
+		north.stroke();
+
+  		
     });
 }
 
 
 
-function rotateArrow(rotateD) {
-
-var a = (-1 * rotateD) * (Math.PI/180);
-
-north.beginPath();
-north.strokeStyle = '#0000ff';
-north.moveTo(x, y);
-north.lineTo(x - Math.sin(a)*60, y - Math.cos(a)*60);
-north.stroke();
-
-}
