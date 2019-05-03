4198ea9c713d0f97bed79f67aa1fd68854e60d99
Popka <foxxoma@gmail.com>
2019-05-03 12:44:47 +0300

    ...

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const canv = document.getElementById('canvas'),
-ctxN = canv.getContext('2d'),
-ctxS = canv.getContext('2d'),
-arc = canv.getContext('2d');
+north = canv.getContext('2d');
+
 
 canv.width = window.innerWidth;
 canv.height = window.innerHeight;
@@ -10,31 +9,20 @@ canv.height = window.innerHeight;
 const x = canv.width / 2, 
 y = canv.height * (85 / 100);
 
-// circle
 
-arc.fillStyle = '#fff';
-arc.arc(x, y, 70, 0, Math.PI * 2);
-arc.fill();
-arc.lineWidth = 5;
-arc.strokeStyle = 'green';
-arc.stroke();
 
 // arrowN
 
-ctxN.beginPath();
-ctxN.strokeStyle = '#0000ff';
-ctxN.moveTo(x, y);
-ctxN.lineTo(x, y - 60);
-ctxN.stroke();
+north.beginPath();
+north.strokeStyle = '#0000ff';
+north.moveTo(x, y);
+north.lineTo(x, y - 60);
+north.stroke();
 
 
-// arrowS
 
-ctxS.beginPath();
-ctxS.strokeStyle = '#ff0000';
-ctxS.moveTo(x, y);
-ctxS.lineTo(x, y + 60);
-ctxS.stroke();
+
+
 
 
 const P = document.getElementById('p');
@@ -49,5 +37,8 @@ if (window.DeviceOrientationEvent) {
     });
 }
 
+/*
+function rotateArrow {
 
-   
+
+}*/
