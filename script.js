7efe6b5c680732f75228f2361c3a589b78e88f20
Popka <foxxoma@gmail.com>
2019-05-04 14:41:56 +0300

    20

--- script.js
+++ script.js
@@ -22,43 +22,18 @@ north.stroke();
 
 
 const P = document.getElementById('p');
-const P2 = document.getElementById('p2');
 
-if (window.DeviceOrientationEvent) {
-    window.addEventListener("deviceorientation", function(event) {
 
-    if(event.absolute == true){
-    	
-        
-       	P.textContent = "true";
+    
 
-       	north.clearRect(0, 0, canv.width, canv.height);
-       
-
-       	let a = (-1 *  event.alpha) * (Math.PI/180);
-       	north.beginPath();
-		north.strokeStyle = '#0000ff';
-		north.moveTo(x, y);
-		north.lineTo(x - Math.sin(a)*60, y - Math.cos(a)*60);
-		north.stroke();
-
-		north.beginPath();
-		north.fillStyle = '#0000ff'	
-		north.arc(x - Math.sin(a)*60, y - Math.cos(a)*60, 5, 0, Math.PI *2);
-		north.fill();
-
-		}
-		else{
-		P.textContent = "f";
-		}
-
+    
 
   		if ('ondeviceorientationabsolute' in window) { 
-  			
-		P2.textContent = "tr2";
-		window.ondeviceorientationabsolute = function(event) { 
 
 		
+		window.ondeviceorientationabsolute = function(event) { 
+
+		P.textContent = event.alpha;
        	
 
        	north.clearRect(0, 0, canv.width, canv.height);
@@ -77,19 +52,41 @@ if (window.DeviceOrientationEvent) {
 		north.fill();
 
 
-		 };
+		};
 		
 		} 
 		else if ('ondeviceorientation' in window) { 
+			
+			window.ondeviceorientationabsolute = function(event) { 
 
-		P2.textContent = "tr22";
+		P.textContent = event.alpha;
+       	
+
+       	north.clearRect(0, 0, canv.width, canv.height);
+       
+
+       	let a = (-1 *  event.alpha) * (Math.PI/180);
+       	north.beginPath();
+		north.strokeStyle = '#0000ff';
+		north.moveTo(x, y);
+		north.lineTo(x - Math.sin(a)*60, y - Math.cos(a)*60);
+		north.stroke();
+
+		north.beginPath();
+		north.fillStyle = '#0000ff'	
+		north.arc(x - Math.sin(a)*60, y - Math.cos(a)*60, 5, 0, Math.PI *2);
+		north.fill();
+
+
+		};
+		
 
 		}
 
 		else{
-			P2.textContent = "f2";
+		P.textContent = "f2";
 		}
-    });
-}
+    
+
 
 
