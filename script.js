60e68bbb011c9a0a8b1216dda8f9d72253ed97eb
Yokio <foxxoma@gamail.com>
2019-05-06 17:57:32 +0400

    1.1

--- script.js
+++ script.js
@@ -2,21 +2,22 @@ const canv = document.getElementById('canvas'),
 north = canv.getContext('2d');
 
 
-canv.width = window.innerWidth;
-canv.height = window.innerHeight;
+canv.width =  Math.sqrt( 2 * Math.pow(window.innerHeight, 2));
+canv.height = Math.sqrt( 2 * Math.pow(window.innerHeight, 2));
 
 
 const x = canv.width / 2, 
 y = canv.height / 2;
 
 
-
-// arrowN
-
+north.translate(x, y);
 
 
 
-const P = document.getElementById('p');
+
+
+
+
 
 
     
@@ -28,7 +29,7 @@ const P = document.getElementById('p');
 		
 		window.ondeviceorientationabsolute = function(event) { 
 
-		P.textContent = event.alpha;
+	
        	
 
        	north.clearRect(0, 0, canv.width, canv.height);
@@ -36,18 +37,21 @@ const P = document.getElementById('p');
 
        	let a = (-1 *  event.alpha) * (Math.PI/180);
        
-       	
+       	north.rotate(a); 
+
        	north.beginPath();
 		north.fillStyle = '#ff0000'	
-		north.arc(x, y, 3, 0, Math.PI *2);
+		north.arc(0, 0, 3, 0, Math.PI *2);
 		north.fill();
 
 		north.beginPath();
 		north.fillStyle = '#0000ff'	
-		north.arc(x - Math.sin(a)*60, y - Math.cos(a)*60, 5, 0, Math.PI *2);
+		north.arc(0 - Math.sin(2*a)*60, 0 - Math.cos(2*a)*60, 5, 0, Math.PI *2);
 		north.fill();
 
+		
 
+		
 		};
 		
 		} 
@@ -55,7 +59,7 @@ const P = document.getElementById('p');
 			
 			window.ondeviceorientationabsolute = function(event) { 
 
-		P.textContent = event.alpha;
+		
        	
 
        	north.clearRect(0, 0, canv.width, canv.height);
@@ -81,7 +85,7 @@ const P = document.getElementById('p');
 		}
 
 		else{
-		P.textContent = "f2";
+		alert("f2");
 		}
     
 
