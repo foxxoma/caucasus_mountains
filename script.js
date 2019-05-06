e12b0d57774067ef02c1581eec2ead64f7f8ab20
Yokio <foxxoma@gamail.com>
2019-05-06 18:10:34 +0400

    1.3

--- script.js
+++ script.js
@@ -13,44 +13,29 @@ y = canv.height / 2;
 north.translate(x, y);
 
 
-
-
-
-
-
-
-
-    
-
-    
-
-  		if ('ondeviceorientationabsolute' in window) { 
-
-		
-		window.ondeviceorientationabsolute = function(event) { 
-
-	
-       	
-
-       	north.clearRect(0, 0, canv.width, canv.height);
-       
-
-       	let a = (-1 *  event.alpha) * (Math.PI/180);
-       
-       	north.rotate(a); 
-
-       	north.beginPath();
+		north.beginPath();
 		north.fillStyle = '#ff0000'	
 		north.arc(0, 0, 3, 0, Math.PI *2);
 		north.fill();
 
 		north.beginPath();
 		north.fillStyle = '#0000ff'	
-		north.arc(0, 0 - 60, 5, 0, Math.PI *2);
+		north.arc(0 , 0 - 60, 5, 0, Math.PI *2);
 		north.fill();
 
-		
 
+
+  		if ('ondeviceorientationabsolute' in window) { 
+
+		
+		window.ondeviceorientationabsolute = function(event) { 
+       
+
+       	let a = (-1 *  event.alpha) * (Math.PI/180);
+       
+       	north.rotate(20); 
+
+    
 		
 		};
 		
@@ -58,27 +43,12 @@ north.translate(x, y);
 		else if ('ondeviceorientation' in window) { 
 			
 			window.ondeviceorientationabsolute = function(event) { 
-
-		
-       	
-
-       	north.clearRect(0, 0, canv.width, canv.height);
        
 
        	let a = (-1 *  event.alpha) * (Math.PI/180);
 
-        north.rotate(a); 
-
-       	north.beginPath();
-		north.fillStyle = '#ff0000'	
-		north.arc(0, 0, 3, 0, Math.PI *2);
-		north.fill();
-
-		north.beginPath();
-		north.fillStyle = '#0000ff'	
-		north.arc(0, 0 - 60, 5, 0, Math.PI *2);
-		north.fill();
-
+       
+			north.rotate(20); 
 
 
 		};
