b1d030fc23b79c2d21d6f32487123513a7279342
Yokio <foxxoma@gamail.com>
2019-05-07 12:34:45 +0400

    2.1

--- script.js
+++ script.js
@@ -2,88 +2,79 @@ const canv = document.getElementById('canvas'),
 north = canv.getContext('2d');
 
 
-canv.width = window.innerWidth;
-canv.height = window.innerHeight;
+canv.width =  Math.sqrt( 2 * Math.pow(window.innerHeight, 2));
+canv.height = Math.sqrt( 2 * Math.pow(window.innerHeight, 2));
 
 
 const x = canv.width / 2, 
 y = canv.height / 2;
 
 
-
-// arrowN
-
+north.translate(x, y);
 
 
 
-const P = document.getElementById('p');
+		north.beginPath();
+		north.fillStyle = '#ff0000'	
+		north.arc(0, 0, 3, 0, Math.PI *2);
+		north.fill();
+
+		north.beginPath();
+		north.fillStyle = '#0000ff'	
+		north.arc(0, -60, 5, 0, Math.PI *2);
+		north.fill();
 
 
-    
-
-    
 
   		if ('ondeviceorientationabsolute' in window) { 
-
 		
-		window.ondeviceorientationabsolute = function(event) { 
+			window.ondeviceorientationabsolute = function(event) {
 
-		P.textContent = event.alpha;
-       	
+       			let a = event.alpha * (Math.PI/180);
+        
+        north.clearRect(-70, -70, 80, 80);
 
-       	north.clearRect(0, 0, canv.width, canv.height);
-       
+		north.rotate(a);
 
-       	let a = (-1 *  event.alpha) * (Math.PI/180);
-       
-       	
-       	north.beginPath();
+		north.beginPath();
 		north.fillStyle = '#ff0000'	
-		north.arc(x, y, 3, 0, Math.PI *2);
+		north.arc(0, 0, 3, 0, Math.PI *2);
 		north.fill();
 
 		north.beginPath();
 		north.fillStyle = '#0000ff'	
-		north.arc(x - Math.sin(a)*60, y - Math.cos(a)*60, 5, 0, Math.PI *2);
+		north.arc(0, -60, 5, 0, Math.PI *2);
 		north.fill();
-
-
-		};
+		
+			};
 		
 		} 
+
 		else if ('ondeviceorientation' in window) { 
 			
-			window.ondeviceorientationabsolute = function(event) { 
+			window.ondeviceorientationabsolute = function(event) {
+       				let a = event.alpha * (Math.PI/180);
+        
+        north.clearRect(-70, -70, 80, 80);
 
-		P.textContent = event.alpha;
-       	
+		north.rotate(a);
 
-       	north.clearRect(0, 0, canv.width, canv.height);
-       
-
-       	let a = (-1 *  event.alpha) * (Math.PI/180);
-
-       	rth.beginPath();
+		north.beginPath();
 		north.fillStyle = '#ff0000'	
-		north.arc(x, y, 3, 0, Math.PI *2);
+		north.arc(0, 0, 3, 0, Math.PI *2);
 		north.fill();
-
 
 		north.beginPath();
 		north.fillStyle = '#0000ff'	
-		north.arc(x - Math.sin(a)*60, y - Math.cos(a)*60, 5, 0, Math.PI *2);
+		north.arc(0, -60, 5, 0, Math.PI *2);
 		north.fill();
-
-
+		
 		};
 		
 
 		}
 
 		else{
-		P.textContent = "f2";
+				alert("f2");
 		}
     
-
-
-
