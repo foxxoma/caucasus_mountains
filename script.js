3ad9535a52a91798d4e79a4fedcf0ee58c3b6693
Yokio <foxxoma@gamail.com>
2019-06-03 11:25:48 +0400

    test9

--- script.js
+++ script.js
@@ -137,8 +137,8 @@ cM = Math.sqrt( Math.pow(bM,2) + Math.pow(aM,2) );
 corner2 = Math.acos( (Math.pow(aM,2) + Math.pow(cM,2) - Math.pow(bM,2)) / (2*aM*cM)) *(180/Math.PI);
 
  s = corner1 + corner2;
-s = s.toFixed(0);
-az = az.toFixed(0);
+
+
 }
 
 else if(yM < y && xM > x){
@@ -150,8 +150,8 @@ cM = Math.sqrt( Math.pow(bM,2) + Math.pow(aM,2) );
 corner2 = Math.acos( (Math.pow(aM,2) + Math.pow(cM,2) - Math.pow(bM,2)) / (2*aM*cM)) *(180/Math.PI);
 
  s = corner1 + 90 + corner2;
-s = s.toFixed(0);
-az = az.toFixed(0);
+
+
 }
 
 else if(yM < y && xM < x){
@@ -163,8 +163,8 @@ cM = Math.sqrt( Math.pow(bM,2) + Math.pow(aM,2) );
 corner2 = Math.acos( (Math.pow(aM,2) + Math.pow(cM,2) - Math.pow(bM,2)) / (2*aM*cM)) *(180/Math.PI);
 
  s = corner1 + 90 + 90 + corner2;
-s = s.toFixed(0);
-az = az.toFixed(0);
+
+
 }
 
 else if (yM > y && xM < x){
@@ -182,8 +182,8 @@ if(s > 360)
 	s = s - 360;
 }
 
-s = s.toFixed(0);
-az = az.toFixed(0);
+
+
 }
 
 
