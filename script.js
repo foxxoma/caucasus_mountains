d592955fc6d2d7dcf803a8dcbda6c3a1579c0ddd
Popka <foxxoma@gmail.com>
2019-08-01 15:42:17 +0300

    ап

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const video = document.getElementById('video');
 
-let ctx = settingEl.canvas.getContext('2d');
+const ctx = settingEl.canvas.getContext('2d');
 
 let front = false;
 let descriptionClick = 0;
@@ -14,8 +14,8 @@ StartCanvasRotateAngle(); //starting angle of view
 
 //I get latitude and longitude
 navigator.geolocation.getCurrentPosition(function(position) {
-	MyPosition.lat = position.coords.latitude / 180 * Math.PI;
-	MyPosition.lng = position.coords.longitude / 180 * Math.PI;
+	MyPosition.lat = position.coords.latitude;
+	MyPosition.lng = position.coords.longitude;
 });
 
 function throttle(callback, delay) {
@@ -56,31 +56,31 @@ if ('ondeviceorientationabsolute' in window) {
 	window.ondeviceorientationabsolute = throttle((event) => {
 		// Check how far the user has scrolled
 		let cornerAz = 360 - event.alpha;
-		angleComparison(cornerAz);
+		mountainInFrontOfMe(cornerAz);
 	}, 30);
 
 } else if ('ondeviceorientation' in window) {
 	window.ondeviceorientationabsolute = throttle((event) => {
 		// Check how far the user has scrolled
 		let cornerAz = 360 - event.alpha;
-		angleComparison(cornerAz);
+		mountainInFrontOfMe(cornerAz);
 	}, 30);
 
 } else {
 	alert("error");
 }
 
-function angleComparison(az) {
+function mountainInFrontOfMe(az) {
 	//Compare azimuth to mountain and current azimuth.
 	descriptionMountainEl.name.textContent = '_____';
 	descriptionMountainEl.contentInDescription.textContent = '_____';
 	descriptionMountainEl.nameInDescription.textContent = '_____';
 
 	for (let f = 0; f < dataMountains.length; f++) {
-		if (Math.abs(az - getAzimuthToMountain(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng)) < viewingAngle) {
+		if (Math.abs(az - getAngleBetweenObjects(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng)) < viewingAngle) {
 			if (lookMountain) {
-				if (distanceComparison(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng) < distanceMountain) {
-					distanceMountain = distanceComparison(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng);
+				if (getDistanceBetweenObjectsn(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng) < distanceMountain) {
+					distanceMountain = getDistanceBetweenObjectsn(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng);
 					descriptionMountainEl.name.textContent = dataMountains[f].name;
 					descriptionMountainEl.contentInDescription.textContent = dataMountains[f].description;
 					descriptionMountainEl.nameInDescription.textContent = dataMountains[f].name;
@@ -90,7 +90,7 @@ function angleComparison(az) {
 				descriptionMountainEl.contentInDescription.textContent = dataMountains[f].description;
 				descriptionMountainEl.nameInDescription.textContent = dataMountains[f].name;
 				lookMountain = true;
-				distanceMountain = distanceComparison(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng);
+				distanceMountain = getDistanceBetweenObjectsn(MyPosition.lat, MyPosition.lng, dataMountains[f].lat, dataMountains[f].lng);
 			}
 		} else {
 			lookMountain = false;
@@ -99,49 +99,52 @@ function angleComparison(az) {
 	}
 }
 
-function distanceComparison(f1, l1, lat2, lng2) {
+function getDistanceBetweenObjectsn(lat1, lng1, lat2, lng2) {
+	const f1 = lat1 / 180 * Math.PI;
+	const l1 = lng1 / 180 * Math.PI;
+	const f2 = lat2 / 180 * Math.PI;
+	const l2 = lng2 / 180 * Math.PI;
 
-	let f2 = lat2 / 180 * Math.PI;
-	let l2 = lng2 / 180 * Math.PI;
-
-	let a = Math.pow(Math.sin((f2 - f1) / 2), 2) + Math.cos(f1) * Math.cos(f2) * Math.pow(Math.sin((l2 - l1) / 2), 2);
-	let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-	let d = 6371 * c;
+	const a = Math.pow(Math.sin((f2 - f1) / 2), 2) + Math.cos(f1) * Math.cos(f2) * Math.pow(Math.sin((l2 - l1) / 2), 2);
+	const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	const d = 6371 * c;
 	return d;
 }
 
 //checks what mountain i look at
-function getAzimuthToMountain(f1, l1, lat2, lng2) {
-	let azimuth;
+function getAngleBetweenObjects(lat1, lng1, lat2, lng2) {
+	let angle;
 
-	let f2 = lat2 / 180 * Math.PI;
-	let l2 = lng2 / 180 * Math.PI;
+	const f1 = lat1 / 180 * Math.PI;
+	const l1 = lng1 / 180 * Math.PI;
+	const f2 = lat2 / 180 * Math.PI;
+	const l2 = lng2 / 180 * Math.PI;
 
-	azimuth = (Math.atan2((Math.sin(l2 - l1) * Math.cos(f2)), (Math.cos(f1) * Math.sin(f2) - Math.sin(f1) * Math.cos(f2) * Math.cos(l2 - l1))));
-	azimuth = azimuth / Math.PI * 180;
+	angle = (Math.atan2((Math.sin(l2 - l1) * Math.cos(f2)), (Math.cos(f1) * Math.sin(f2) - Math.sin(f1) * Math.cos(f2) * Math.cos(l2 - l1))));
+	angle = angle / Math.PI * 180;
 
-	if (azimuth < 0) {
-		azimuth = 360 - azimuth;
+	if (angle < 0) {
+		angle = 360 - angle;
 	}
 
 	//degree check with upside down screen 
 	if (window.orientation == 90 || window.orientation == -90) {
-		azimuth = azimuth - 90;
-		if (azimuth < 0) {
-			azimuth = 360 - azimuth;
+		angle = angle - 90;
+		if (angle< 0) {
+			angle = 360 - angle;
 		}
 	}
 
-	return azimuth;
+	return angle;
 
 }
 
 function StartCanvasRotateAngle() {
-	let a = 5 * Math.PI / 180;
+	const a = 5 * Math.PI / 180;
 	let radrot = settingEl.canvas.height - (settingEl.canvas.height / 100) * 10;
 
-	let xCStart = settingEl.canvas.width / 2;
-	let yCStart = settingEl.canvas.height;
+	const xCStart = settingEl.canvas.width / 2;
+	const yCStart = settingEl.canvas.height;
 
 	ctx.clearRect(0, 0, settingEl.canvas.width, settingEl.canvas.height);
 	ctx.fillStyle = 'magenta';
@@ -158,11 +161,11 @@ function StartCanvasRotateAngle() {
 
 settingEl.range.angle.oninput = function(e) {
 	viewingAngle = e.target.value / 2;
-	let a = e.target.value / 2 * Math.PI / 180;
+	const a = e.target.value / 2 * Math.PI / 180;
 	let radrot = settingEl.canvas.height - (settingEl.canvas.height / 100) * 10;
 
-	let xCStart = settingEl.canvas.width / 2;
-	let yCStart = settingEl.canvas.height;
+	const xCStart = settingEl.canvas.width / 2;
+	const yCStart = settingEl.canvas.height;
 
 	ctx.clearRect(0, 0, settingEl.canvas.width, settingEl.canvas.height);
 	ctx.fillStyle = 'magenta';
